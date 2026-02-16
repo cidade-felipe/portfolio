0c08f540d6b3a92e1ee452f3a7368df3982340d3
felipecidade94 <felipecidade94@gmail.com>
2026-02-16 17:53:16 -0300

    commit

--- python/converter.py
+++
@@ -1,9 +0,0 @@
-from markitdown import MarkItDown
-import re
-
-md = MarkItDown()
-
-resultado = md.convert(r"pdf\Currículo_Felipe_Cidade.pdf")
-
-with open(r"md\Currículo_Felipe_Cidade.md", "w", encoding="utf-8") as f:
-   f.write(resultado.text_content)
